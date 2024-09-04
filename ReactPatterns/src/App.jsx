1753c9f9242c10ccbe21ebc8a772ae285d7e0dc0
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-04 11:45:50 +0300

    Add the search logic to a SearchableList

--- ReactPatterns/src/App.jsx
+++ ReactPatterns/src/App.jsx
@@ -1,34 +1,83 @@
-import Accordion from "./components/Accordion";
+import Accordion from "./components/Accordion/Accordion";
+import SearchableList from "./components/SearchableList/SearchableList";
+
+import savannaImg from "./assets/african-savanna.jpg";
+import amazonImg from "./assets/amazon-river.jpg";
+import caribbeanImg from "./assets/caribbean-beach.jpg";
+import desertImg from "./assets/desert-dunes.jpg";
+import forestImg from "./assets/forest-waterfall.jpg";
+
+const PLACES = [
+  {
+    id: "african-savanna",
+    image: savannaImg,
+    title: "African Savanna",
+    description: "Experience the beauty of nature.",
+  },
+  {
+    id: "amazon-river",
+    image: amazonImg,
+    title: "Amazon River",
+    description: "Get to know the largest river in the world.",
+  },
+  {
+    id: "caribbean-beach",
+    image: caribbeanImg,
+    title: "Caribbean Beach",
+    description: "Enjoy the sun and the beach.",
+  },
+  {
+    id: "desert-dunes",
+    image: desertImg,
+    title: "Desert Dunes",
+    description: "Discover the desert life.",
+  },
+  {
+    id: "forest-waterfall",
+    image: forestImg,
+    title: "Forest Waterfall",
+    description: "Listen to the sound of the water.",
+  },
+];
 
 function App() {
   return (
     <main>
-      <h1>React Patterns & Practices</h1>
-      <Accordion className="accordion">
-        <Accordion.Item className="accordion-item">
-          <Accordion.Title className="accordion-item-title" id="experience">
-            10+ years of experience
-          </Accordion.Title>
-          <Accordion.Content className="accordion-item-content" id="experience">
-            <article>
-              <p>This should be bla.. bla.. bla...</p>
-            </article>
-          </Accordion.Content>
-        </Accordion.Item>
-        <Accordion.Item className="accordion-item">
-          <Accordion.Title id="local-guides" className="accordion-item-title">
-            We are working hahahahah
-          </Accordion.Title>
-          <Accordion.Content
-            id="local-guides"
-            className="accordion-item-content"
-          >
-            <article>
-              <p>This should be bla.. bla.. bla...</p>
-            </article>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion>
+      <section>
+        <h1>React Patterns & Practices</h1>
+        <Accordion className="accordion">
+          <Accordion.Item className="accordion-item">
+            <Accordion.Title className="accordion-item-title" id="experience">
+              10+ years of experience
+            </Accordion.Title>
+            <Accordion.Content
+              className="accordion-item-content"
+              id="experience"
+            >
+              <article>
+                <p>This should be bla.. bla.. bla...</p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item className="accordion-item">
+            <Accordion.Title id="local-guides" className="accordion-item-title">
+              We are working hahahahah
+            </Accordion.Title>
+            <Accordion.Content
+              id="local-guides"
+              className="accordion-item-content"
+            >
+              <article>
+                <p>This should be bla.. bla.. bla...</p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+      <section>
+        <SearchableList items={PLACES} />
+        <SearchableList items={["item 1", "item 2"]} />
+      </section>
     </main>
   );
 }
---
+++ ReactPatterns/src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function SearchableList({ items }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchResult = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+    console.log(searchTerm);
+  }
+
+  return (
+    <div className="searchable-list">
+      <input type="search" placeholder="Search" onChange={handleChange} />
+      <ul>
+        {searchResult.map((item, index) => (
+          <li key={index}>{item.toString()}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
